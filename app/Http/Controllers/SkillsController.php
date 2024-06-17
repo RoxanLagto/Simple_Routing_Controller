53dfa3521cf55f92a43ae71333e4ed10f26fed33
RoxanLagto <rpl1771@students.uc-bcf.edu.ph>
2024-06-18 01:32:22 +0800

    Deleted: Past views and controllers Created: New controllers and views.

--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\PersonalRoutes;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SkillsController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\SkillsController;
 use App\Http\Controllers\HobbiesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/AboutMe', [AboutMeController:: class, 'index'])->name('about');
-Route::get('/Skills', [SkillsController:: class, 'index'])->name('skills');
-Route::get('/Hobbies', [HobbiesController:: class, 'index'])->name('hobbies');
+Route::get('/AboutMe', [AboutMeController:: class, 'about']);
+Route::get('/Skills', [SkillsController:: class, 'skills']);
+Route::get('/Hobbies', [HobbiesController:: class, 'hobbies']);
 
 
