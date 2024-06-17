a056edfa7a767e66bbe71a1fb63335d02c4897bd
RoxanLagto <rpl1771@students.uc-bcf.edu.ph>
2024-06-18 00:03:48 +0800

    Create: Simple Routing Controller Project

---
+++ app/Http/Controllers/PersonalRoutes/HobbiesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\PersonalRoutes;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HobbiesController extends Controller
+{
+    //
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/AboutMe', function () {
+    return view('welcome');
+});
+
+
+Route::get('/Skills', function () {
+    return view('welcome');
+});
+
+
+Route::get('/Hobbies', function () {
+    return view('welcome');
+});
+
+
