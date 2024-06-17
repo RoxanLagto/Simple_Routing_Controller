29565a5afd2bdfe37cc07adf5141abef1b44f63c
RoxanLagto <rpl1771@students.uc-bcf.edu.ph>
2024-06-18 00:31:34 +0800

    Create: Routes (About Me, Skills, Hobbies)

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\HobbiesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/AboutMe', function () {
-    return view('welcome');
-});
-
-
-Route::get('/Skills', function () {
-    return view('welcome');
-});
-
-
-Route::get('/Hobbies', function () {
-    return view('welcome');
-});
+Route::get('/AboutMe', [AboutMeController:: class, 'index'])->name('about');
+Route::get('/Skills', [SkillsController:: class, 'index'])->name('skills');
+Route::get('/Hobbies', [HobbiesController:: class, 'index'])->name('hobbies');
 
 
