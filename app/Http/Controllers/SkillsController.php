92cfb10a5d917ddd46b1571eab55a37467f8e62a
RoxanLagto <rpl1771@students.uc-bcf.edu.ph>
2024-06-19 00:13:43 +0800

    Update: Added some information on skills. blade and skillsController

--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -8,10 +8,16 @@ class SkillsController extends Controller
 {
     public function skills()
     {
-        
-
-        return view('skills');
-        
+        return view('skills')
+                ->with('skill1' , '•    Knowledge in programming/ scripting language: Low code (ADALO), Java, Python, HTML, CSS')
+                ->with('skill2' , '•    Knowledgeable in MS Office Tools: Word, Excel, PowerPoint')
+                ->with('skill3' , '•	Proficient in utilizing Canva, a graphic design platform')
+                ->with('skill4' , '•	Knowledgeable in using Figma, a prototyping platform')
+                ->with('skill5' , '•	Proven leadership ability and quality as evidenced in various school events')
+                ->with('skill6' , '•	Flexible individual and team player who functions well in a fast-paced work environment')
+                ->with('skill7' , '•	A person with a positive attitude as well as a professional and helpful demeanor')
+                ->with('skill8' , '•	Proficient in communication skills and public speaking')
+                ->with('skill9' , '•	Open-minded and dedicated to gaining new skills and knowledge for improvement in the workplace');
 
     }
 }
--- resources/views/Skills.blade.php
+++ resources/views/Skills.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Me</title>
+</head>
+<body style="background-color: black">
+    <div class="buttons" style="text-align: center">
+        <a href="{{ route('about') }}"><button style="padding: 10px; border: none; border-radius: 5px; background-color:white">About Me</button></a>
+        <a href="{{ route('skills') }}"><button style="padding: 10px; border: none; border-radius: 5px; background-color:white">Skills</button></a>
+        <a href="{{ route('hobbies') }}"><button style="padding: 10px; border: none; border-radius: 5px; background-color:white">Hobbies</button></a>
+    </div>
+
+    <div class="personal_details" style="margin-left: 50px; color: white">
+        <h1>SKILLS</h1>
+        <p> {{ $skill1 }}</p>
+        <p> {{ $skill2  }}</p>
+        <p> {{ $skill3  }}</p>
+        <p> {{ $skill4  }}</p>
+        <p> {{ $skill5 }}</p>
+        <p> {{ $skill6  }}</p>
+        <p> {{ $skill7  }}</p>
+        <p> {{ $skill8  }}</p>
+        <p> {{ $skill9  }}</p>
+    </div>
+</body>
+</html>
