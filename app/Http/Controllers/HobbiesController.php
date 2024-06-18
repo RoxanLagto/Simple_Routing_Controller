78c60323829d5ba39124674569384844e9adc8d3
RoxanLagto <rpl1771@students.uc-bcf.edu.ph>
2024-06-19 00:39:01 +0800

    Update: Added info on HobbiesController and hobbies.blade with css

--- app/Http/Controllers/HobbiesController.php
+++ app/Http/Controllers/HobbiesController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class HobbiesController extends Controller
 {
-    //
+    public function hobbies()
+    {
+        return view('hobbies')
+                ->with('hobby1' , '>    Watching Movies')
+                ->with('hobby2' , '>    Sleeping')
+                ->with('hobby3' , '>    Traveling')
+                ->with('hobby4' , '>	Going to different cafes')
+                ->with('hobby5' , '>	Eating');
+
+    }
 }
--- resources/views/Hobbies.blade.php
+++ resources/views/Hobbies.blade.php
@@ -1,3 +1,24 @@
-<div>
-    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hobbies</title>
+</head>
+<body style="background-color: black">
+    <div class="buttons" style="text-align: center">
+        <a href="{{ route('about') }}"><button style="padding: 10px; border: none; border-radius: 5px; background-color:white">About Me</button></a>
+        <a href="{{ route('skills') }}"><button style="padding: 10px; border: none; border-radius: 5px; background-color:white">Skills</button></a>
+        <a href="{{ route('hobbies') }}"><button style="padding: 10px; border: none; border-radius: 5px; background-color:white">Hobbies</button></a>
+    </div>
+
+    <div class="hobbies_details" style="margin-left: 50px; color: white">
+        <h1>HOBBIES</h1>
+        <p> {{ $hobby1 }}</p>
+        <p> {{ $hobby2 }}</p>
+        <p> {{ $hobby3 }}</p>
+        <p> {{ $hobby4 }}</p>
+        <p> {{ $hobby5 }}</p>
+    </div>
+</body>
+</html>
